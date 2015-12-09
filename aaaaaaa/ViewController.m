8704c6d46933fe7103b81f01042b6838a8d2921b
LiNa_L <546278334@qq.com>
2015-12-09 15:14:00 +0800

    1234

---
+++ aaaaaaa/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  aaaaaaa
+//
+//  Created by LiNa on 15/12/9.
+//  Copyright © 2015年 Lina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"成功");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ aaaaaaa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aaaaaaa
+//
+//  Created by LiNa on 15/12/9.
+//  Copyright © 2015年 Lina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
